d413490a43677a050adab1e307702de7c61e4c00
aharris4455 <augieharris9@gmail.com>
2024-12-22 13:57:18 +0200

    Added PSD spectrum sensing

--- MATLAB/sdr.m
+++ MATLAB/sdr.m
@@ -1,25 +1,38 @@
 %% Frequency hopping for RTL-SDR
 clear; 
+fs = 2.4e6;
+threshold = -30; % PSD Threshold for detection, dB
+iterTime = 1; % Time in seconds for one iteration
 centerFreq = [939515400 941515400 943515400 945515400 947515400]; %GSM freq
+%centerFreq = 433000000:2e6:947515400; 
+%centerFreq = 433000000;
 rx = comm.SDRRTLReceiver("CenterFrequency",centerFreq(1), ...
-                         "SampleRate", 2.4e6, "OutputDataType", "double", ...
+                         "SampleRate", fs, "OutputDataType", "double", ...
                          "SamplesPerFrame", 1024);
-disp('Receiving GSM signal...');
-scope = spectrumAnalyzer('SampleRate', 2.4e6);
-disp('Displaying spectrum...');
+scope = spectrumAnalyzer('SampleRate', fs);
 try
     while true
         for i = centerFreq
             rx.CenterFrequency = i;
-            disp(i);
             startTime = tic;
-            while toc(startTime) < 1
+            while toc(startTime) < iterTime
                 [rxSignal, ~] = rx();
-                scope.Title = ['Freq: ', num2str(i/1e6), 'MHz'];
-                scope(rxSignal)
-            end
-        end
-    end
+                [psd, ~] = pwelch(rxSignal, hamming(1024),[],[],fs,'power');
+                avgPSD = 10*log10(mean(psd));
+                disp(['PSD of Channel: ', num2str(i/1e6), ...
+                            'MHz = ', num2str(avgPSD)]);
+                if avgPSD < threshold 
+                    disp(['Channel: ',num2str(i/1e6), ' Free']);
+                    toggleSwitch = 0;
+                else
+                    disp(['Channel: ', num2str(i/1e6) ,' in use']);
+                    toggleSwitch = 1;
+                end
+                %scope.Title = ['Freq: ', num2str(i/1e6), 'MHz'];
+                %scope(rxSignal)
+            end % Toc loop
+        end % Freq loop
+    end % Sim loop
 catch ME
     disp(ME)
     release(scope);
