9d20481144f7f20fea620128dbd2ae92f40ed352
aharris4455 <134221142+aharris4455@users.noreply.github.com>
2025-01-18 21:50:22 +0200

    Add files via upload
    Added simulation

--- MATLAB/sdr.m
+++ MATLAB/sdr.m
@@ -2,10 +2,14 @@
 clear; 
 fs = 2.4e6;
 threshold = -30; % PSD Threshold for detection, dB
-iterTime = 1; % Time in seconds for one iteration
-centerFreq = [939515400 941515400 943515400 945515400 947515400]; %GSM freq
-%centerFreq = 433000000:2e6:947515400; 
+iterTime = 0.001; % Time in seconds for one iteration
+%centerFreq = [939515400 941515400 943515400 945515400 947515400]; %GSM freq
+%centerFreq = 433000000:2e6:1700000000; 
+centerFreq = 222e6;
 %centerFreq = 433000000;
+%centerFreq =  949200000;
+%centerFreq = 857000000;
+%795 000 000; 
 rx = comm.SDRRTLReceiver("CenterFrequency",centerFreq(1), ...
                          "SampleRate", fs, "OutputDataType", "double", ...
                          "SamplesPerFrame", 1024);
@@ -28,8 +32,8 @@ try
                     disp(['Channel: ', num2str(i/1e6) ,' in use']);
                     toggleSwitch = 1;
                 end
-                %scope.Title = ['Freq: ', num2str(i/1e6), 'MHz'];
-                %scope(rxSignal)
+                scope.Title = ['Freq: ', num2str(i/1e6), 'MHz'];
+                scope(rxSignal)
             end % Toc loop
         end % Freq loop
     end % Sim loop
